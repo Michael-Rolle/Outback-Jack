06e01c675f3e5c6288ab6e431faf3f87708b5099
Michael Rolle <michaelrolle7@gmail.com>
2022-09-13 23:23:13 +0200

    Initialized Window

--- game-source-code/main.cpp
+++ game-source-code/main.cpp
@@ -6,9 +6,25 @@
 
 using namespace std;
 
+// Global Constants
+const int gameWidth = 1280;
+const int gameHeight = 720;
+const int frameRate = 60;
+
 int main()
 {
+    sf::RenderWindow window(sf::VideoMode(gameWidth, gameHeight), "Outback Jack");
+    window.setFramerateLimit(frameRate);
 
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed) window.close();
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) window.close();
+        }
+    }
 
     return 0;
 }
