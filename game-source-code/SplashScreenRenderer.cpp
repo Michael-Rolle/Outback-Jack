c5bfae1db55ed31cdaca6eee4bde9b15fca1783a
Brayden Hand <brayden.hand@gmail.com>
2022-09-21 17:21:01 +0200

    Finished splash screen controls

--- game-source-code/SplashScreenRenderer.cpp
+++ game-source-code/SplashScreenRenderer.cpp
@@ -27,7 +27,7 @@ SplashScreenRenderer::SplashScreenRenderer(const float gameWidth, const float ga
     sf::FloatRect titleRect = title.getLocalBounds();
     sf::FloatRect startRect = startMessage.getLocalBounds();
     title.setOrigin(titleRect.left + titleRect.width/2.0f, titleRect.top + titleRect.height/2.0f);
-    title.setPosition(gameWidth/2, gameHeight/2 - title.getCharacterSize());
+    title.setPosition(gameWidth/2, gameHeight/2 - 3.5*title.getCharacterSize());
     startMessage.setOrigin(startRect.left + startRect.width/2.0f, startRect.top + startRect.height/2.0f);
     startMessage.setPosition(title.getPosition().x, title.getPosition().y + 2*title.getCharacterSize());
 
@@ -42,7 +42,7 @@ SplashScreenRenderer::SplashScreenRenderer(const float gameWidth, const float ga
 
     //Controls image
     if(!controls_1.loadFromFile("resources/wasd.png"))
-        throw "cannot load one player controls image";
+        throw "cannot load player ones controls image";
     controls_1_Image.setTexture(controls_1);
     controls_1_Image.setScale(gameWidth/(6.0f*controls_1_Image.getLocalBounds().width), gameHeight/(5.0f*controls_1_Image.getLocalBounds().height));
     sf::FloatRect wasdRect = controls_1_Image.getLocalBounds();
@@ -50,16 +50,16 @@ SplashScreenRenderer::SplashScreenRenderer(const float gameWidth, const float ga
     controls_1_Image.setPosition(gameWidth/9.0f, gameHeight/1.25f);
 
     if(!controls_2.loadFromFile("resources/arrows.png"))
-        throw "cannot load one player controls image";
+        throw "cannot load player two controls image";
     controls_2_Image.setTexture(controls_2);
     controls_2_Image.setScale(gameWidth/(6.0f*controls_2_Image.getLocalBounds().width), gameHeight/(5.0f*controls_2_Image.getLocalBounds().height));
     sf::FloatRect arrowsRect = controls_2_Image.getLocalBounds();
     controls_2_Image.setOrigin(arrowsRect.left + arrowsRect.width/2.0f, arrowsRect.top + arrowsRect.height/2.0f);
-    controls_2_Image.setPosition(gameWidth/1.125f, gameHeight/1.25f);
+    controls_2_Image.setPosition(gameWidth/1.13f, gameHeight/1.25f);
 
     //Controls text
     player_1.setFont(font);
-    player_1.setCharacterSize(15);
+    player_1.setCharacterSize(20);
     player_1.setFillColor(sf::Color::White);
     player_1.setString("PLAYER  1  CONTROLS");
     player_1.setLetterSpacing(3);
@@ -70,7 +70,7 @@ SplashScreenRenderer::SplashScreenRenderer(const float gameWidth, const float ga
     player_1.setPosition(gameWidth/9, gameHeight/1.6);
 
     player_2.setFont(font);
-    player_2.setCharacterSize(15);
+    player_2.setCharacterSize(20);
     player_2.setFillColor(sf::Color::White);
     player_2.setString("PLAYER  2  CONTROLS");
     player_2.setLetterSpacing(3);
@@ -78,7 +78,29 @@ SplashScreenRenderer::SplashScreenRenderer(const float gameWidth, const float ga
     player_2.setOutlineColor(sf::Color::Red);
     sf::FloatRect player_2_Rect = player_2.getLocalBounds();
     player_2.setOrigin(player_2_Rect.left + player_2_Rect.width/2.0f, player_2_Rect.top + player_2_Rect.height/2.0f);
-    player_2.setPosition(gameWidth/1.125f, gameHeight/1.6);
+    player_2.setPosition(gameWidth/1.13f, gameHeight/1.6);
+
+    controls_1_text.setFont(font);
+    controls_1_text.setCharacterSize(15);
+    controls_1_text.setFillColor(sf::Color::Black);
+    controls_1_text.setString(" \t\t\t\t\t\t\t\t\t\t\t\t  JUMP  UP  \n\n\n\n\n\n\n     MOVE  LEFT \t\t\t\t\t\t\t\t\t\t MOVE  RIGHT  \n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t\t\t\t  JUMP  DOWN");
+    controls_1_text.setLetterSpacing(3);
+    controls_1_text.setOutlineThickness(3);
+    controls_1_text.setOutlineColor(sf::Color::White);
+    sf::FloatRect controls_1_text_Rect = controls_1_text.getLocalBounds();
+    controls_1_text.setOrigin(controls_1_text_Rect.left + controls_1_text_Rect.width/2.0f, controls_1_text_Rect.top + controls_1_text_Rect.height/2.0f);
+    controls_1_text.setPosition(gameWidth/9, gameHeight/1.25);
+
+    controls_2_text.setFont(font);
+    controls_2_text.setCharacterSize(15);
+    controls_2_text.setFillColor(sf::Color::Black);
+    controls_2_text.setString(" \t\t\t\t\t\t\t\t\t\t\t\t  JUMP  UP  \n\n\n\n\n\n\n     MOVE  LEFT \t\t\t\t\t\t\t\t\t\t MOVE  RIGHT  \n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t\t\t\t  JUMP  DOWN");
+    controls_2_text.setLetterSpacing(3);
+    controls_2_text.setOutlineThickness(3);
+    controls_2_text.setOutlineColor(sf::Color::White);
+    sf::FloatRect controls_2_text_Rect = controls_2_text.getLocalBounds();
+    controls_2_text.setOrigin(controls_2_text_Rect.left + controls_2_text_Rect.width/2.0f, controls_2_text_Rect.top + controls_2_text_Rect.height/2.0f);
+    controls_2_text.setPosition(gameWidth/1.13f, gameHeight/1.25);
 }
 
 void SplashScreenRenderer::renderSplashScreen(sf::RenderWindow& window)
@@ -86,8 +108,8 @@ void SplashScreenRenderer::renderSplashScreen(sf::RenderWindow& window)
     window.draw(backgroundImage);
     window.draw(title);
     window.draw(startMessage);
-    //window.draw(controls_1_text);
-    //window.draw(controls_2_text);
+    window.draw(controls_1_text);
+    window.draw(controls_2_text);
     window.draw(player_1);
     window.draw(player_2);
     window.draw(controls_1_Image);
