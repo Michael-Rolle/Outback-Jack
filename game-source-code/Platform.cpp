b14800bc839e225e7eb1af89f2d66da9e50bf493
Michael Rolle <michaelrolle7@gmail.com>
2022-09-25 20:25:12 +0200

    constructor complete

--- game-source-code/Platform.cpp
+++ game-source-code/Platform.cpp
@@ -1,7 +1,7 @@
 #include "platform.h"
 #include <SFML/Graphics.hpp>
 
-Platform::Platform(const float gameWidth, const float gameHeight)
+/*Platform::Platform(const float gameWidth, const float gameHeight)
 {
     //Playing field image
     if(!platform.loadFromFile("resources/log.png"))
@@ -11,14 +11,33 @@ Platform::Platform(const float gameWidth, const float gameHeight)
     sf::FloatRect platformRect = platformImage.getLocalBounds();
     platformImage.setOrigin(platformRect.left + platformRect.width/2.0f, platformRect.top + platformRect.height/2.0f);
     platformImage.setPosition(gameWidth/2.0f, gameHeight/1.7f);
-}
+}*/
 
-Platform::isActive(Jack jack)
+Platform::Platform(sf::Texture* texture, float speed, bool movingRight, bool gameRow)
 {
-    return 0;
+    this->speed = speed;
+    this->movingRight = movingRight;
+    this->gameRow = gameRow;
+    platform.setTexture(*texture);
+    platform.scale(height/platform.getLocalBounds().width, height/platform.getLocalBounds().height);
+    platform.setOrigin(platform.getLocalBounds().width/2, platform.getLocalBounds().top); //So the platform will spawn at the bottom of the game row.
+    if(movingRight)
+        platform.setPosition(0+platform.getGlobalBounds()/2, (gameRow*180)+90);
+    else
+        platform.setPosition(1080-platform.getGlobalBounds()/2, (gameRow*180)+90);
 }
 
-void Platform::renderPlatform(sf::RenderWindow& window)
+void Platform::changeDirection()
+{
+    movingRight = !movingRight;
+}
+
+void Platform::update(float deltaTime)
+{
+
+}
+
+void Platform::draw(sf::RenderWindow& window)
 {
     window.draw(platformImage);
 }
--- game-source-code/Platform.h
+++ game-source-code/Platform.h
@@ -7,13 +7,14 @@
 class Platform
 {
     public:
-        Platform(sf::Texture* texture, float speed, bool movingRight);
+        Platform(sf::Texture* texture, float speed, bool movingRight, bool gameRow);
         void changeDirection();
         void update(float deltaTime);
         void draw(sf::RenderWindow& window);
         //isActive(Jack jack);
         //void renderPlatform(sf::RenderWindow& window);
 
+    private:
         //bool active;
         //sf::Texture platform;
         sf::Sprite platform;
@@ -21,6 +22,8 @@ class Platform
         unsigned int gameRow;
         float speed;
         sf::Vector2f velocity;
+        const float height = 90;
+        const float width = 66.77;
 };
 
 #endif
