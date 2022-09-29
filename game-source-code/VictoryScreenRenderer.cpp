8dafba3b8a0b4add543f0e688b6997e5280daf99
Michael Rolle <michaelrolle7@gmail.com>
2022-09-29 21:25:28 +0200

    started working on vicotry screen

--- game-source-code/VictoryScreenRenderer.cpp
+++ game-source-code/VictoryScreenRenderer.cpp
@@ -1,6 +1,20 @@
 #include "VictoryScreenRenderer.h"
 
-VictoryScreenRenderer::VictoryScreenRenderer()
+VictoryScreenRenderer::VictoryScreenRenderer(sf::Font* font)
 {
-    //ctor
+    winText.setFont(*font);
+    restartText.setFont(*font);
+    winText.setString("You Win!");
+    restartText.setString("Press Space to Replay");
+    winText.setOrigin(winText.getLocalBounds().width/2.0f, winText.getLocalBounds().height/2.0f);
+    restartText.setOrigin(restartText.getLocalBounds().width/2.0f, restartText.getLocalBounds().height/2.0f);
+    winText.setPosition(1920/2.0f, 0.3*1080);
+    restartText.setPosition(1920/2.0f, 0.6*1080);
+    winText.setScale(0.33.0f*1920.0f/winText.getLocalBounds().width, 0.33.0f*1920.0f/winText.getLocalBounds().height)
+}
+
+void VictoryScreenRenderer::draw(sf::RenderWindow& window)
+{
+    window.draw(winText);
+    window.draw(restartText);
 }
