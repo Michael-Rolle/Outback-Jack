180bd6dee87266f082b4f4fea654ad8d769c9f07
Michael Rolle <michaelrolle7@gmail.com>
2022-10-04 11:11:22 +0200

    public interface complete

--- game-source-code/Kangaroo.h
+++ game-source-code/Kangaroo.h
@@ -1,13 +1,25 @@
 #ifndef KANGAROO_H
 #define KANGAROO_H
+#include <SFML/Graphics.hpp>
+#include "Jack.h"
+#include "Animation.h"
 
 
 class Kangaroo
 {
     public:
-        Kangaroo();
+        Kangaroo(sf::Texture* spritesheet, float height, float speed);
+        float getPositionX(){ return joey.getPosition().x; }
+        float width(){ return joey.getGlobalBounds().width; }
+        void update(Jack& player);
+        void draw(sf::RenderWindow& window);
 
     private:
+        Animation animation;
+        sf::Sprite joey;
+        float height;
+        float speed;
+        bool movingRight;
 };
 
 #endif // KANGAROO_H
