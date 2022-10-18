340ded0ba344cc110a1a41ed8819045f4582c658
Brayden Hand <brayden.hand@gmail.com>
2022-10-18 21:28:05 +0200

    Update_7

--- game-source-code/PlatformController.h
+++ game-source-code/PlatformController.h
@@ -3,6 +3,10 @@
 #include "PlatformRow.h"
 #include <vector>
 
+/*!
+Class capable of creating, updating and reading each platform row (and hence each platform as well) that exists.
+*/
+
 using namespace std;
 
 class PlatformController
@@ -10,12 +14,12 @@ class PlatformController
     public:
         PlatformController(sf::Texture* texture);
         PlatformController(){};
-        void changePlatformRowColour(unsigned int row, sf::Texture* texture, bool original); //enter a row num from 1 to 4
-        void update(float deltaTime);
-        void draw(sf::RenderWindow& window);
-        bool allPlatformsNewColour();
-        PlatformRow* getPlatformRow(const unsigned int row); //enter a row num from 1 to 4
-        vector<float> getPlatformPositions(const unsigned int row); //enter a row num from 1 to 4
+        void changePlatformRowColour(unsigned int row, sf::Texture* texture, bool original); /*!<  */
+        void update(float deltaTime); /*!<  */
+        void draw(sf::RenderWindow& window); /*!<  */
+        bool allPlatformsNewColour(); /*!<  */
+        PlatformRow* getPlatformRow(const unsigned int row); /*!<  */
+        vector<float> getPlatformPositions(const unsigned int row); /*!<  */
 
     private:
         vector<PlatformRow> platformRows;
