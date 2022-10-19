3c19457e0475f111839dc0179cd26cc60aeff14c
Brayden Hand <brayden.hand@gmail.com>
2022-10-19 15:37:51 +0200

    Update4

--- game-source-code/Tent.h
+++ game-source-code/Tent.h
@@ -2,17 +2,22 @@
 #define TENT_H
 #include <SFML/Graphics.hpp>
 
+/*!
+Visually and incremementally, constructs/deconstructs a tent using member functions. The tent cannot be deconstructed if none of it has
+been built and stops being being constructed if it is already fully built.
+*/
+
 class Tent
 {
     public:
         Tent(sf::Texture* texture, unsigned int rows, unsigned int columns, float height);
         Tent(){};
-        void nextFrame();
-        void removeFrame();
-        float getPositionX();
-        float width();
-        void draw(sf::RenderWindow& window);
-        void reset();
+        void nextFrame(); /*!< Adds the next frame to the tent if it is not fully built. Frames are added horizontally and then vertically. */
+        void removeFrame(); /*!< Removes the most recent frame from the tent while it is not fully deconstructed. */
+        float getPositionX(); /*!< Returns the horizontal position of the tents' sprite as a float. */
+        float width(); /*!< Returns the global width of the tents' sprite as a float. */
+        void draw(sf::RenderWindow& window); /*!< Draws the current state of the tent including all constructed frames. */
+        void reset(); /*!< Fully deconstructs the tent by resetting its frames. */
 
     public:
         bool built;
--- game-source-code/VictoryScreenRenderer.h
+++ game-source-code/VictoryScreenRenderer.h
@@ -2,11 +2,16 @@
 #define VICTORYSCREENRENDERER_H
 #include <SFML/Graphics.hpp>
 
+/*!
+Draws the victory screen for when the player wins the game.
+*/
+
 class VictoryScreenRenderer
 {
     public:
         VictoryScreenRenderer(const float gameWidth, const float gameHeight);
-        void draw(sf::RenderWindow& window);
+        void draw(sf::RenderWindow& window); /*!< Draws the text notifying the player they have won and the game can be restarted
+        by pressing space, with reference to a RenderWindow. */
 
     private:
         sf::Font font;
