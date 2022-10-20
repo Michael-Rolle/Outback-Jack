8e02a9120d82bd92c23c94e9dbae4126f7745ea2
Brayden Hand <brayden.hand@gmail.com>
2022-10-20 16:10:45 +0200

    Update4

--- game-source-code/Platform.h
+++ game-source-code/Platform.h
@@ -3,8 +3,7 @@
 #include <SFML/Graphics.hpp>
 
 /*!
-Creates a Platform object capable of horizontal movement, being able to change colour and being able to change direction
-when the player presses the shift key.
+Creates a Platform object capable of horizontal movement, being able to change colour and being able to change direction when the player presses the shift key.
 */
 
 class Platform
@@ -12,15 +11,20 @@ class Platform
     public:
         Platform(sf::Texture* texture, float speed, bool movingRight, unsigned int gameRow);
         ~Platform(){}
-        void changeDirection(); /*!< Reverses the Platform objects' current direction. */
-        void changeColour(sf::Texture* texture); /*!< Changes the Platform objects' current texture to the texture specified
-                                                      in the parameter. */
-        void setPositionX(float x); /*!< Sets the Platform objects' horizontal position to that specified by the float parameter
-                                         ,without changing its vertical position.*/
-        float getPositionX(); /*!< Returns the horizontal position of the Platform object as a float. */
-        void update(float deltaTime); /*!< Updates the Platform object to move at a constant speed, either left or right. */
-        void draw(sf::RenderWindow& window); /*!< Draws the Platform object with referencce to a RenderWindow. */
-        float width(); /*!< Returns the global width of the Platform object as a float. */
+        void changeDirection();
+        /*!< Reverses the Platform objects' current direction. */
+        void changeColour(sf::Texture* texture);
+        /*!< Changes the Platform objects' current texture to the texture specified in the parameter. */
+        void setPositionX(float x);
+        /*!< Sets the Platform objects' horizontal position to that specified by the float parameter, without changing its vertical position. */
+        float getPositionX();
+        /*!< Returns the horizontal position of the Platform object as a float. */
+        void update(float deltaTime);
+        /*!< Updates the Platform object to move at a constant speed, either left or right. */
+        void draw(sf::RenderWindow& window);
+        /*!< Draws the Platform object with referencce to a RenderWindow. */
+        float width();
+        /*!< Returns the global width of the Platform object as a float. */
 
     private:
         sf::Sprite platform;
--- game-source-code/PlatformRow.h
+++ game-source-code/PlatformRow.h
@@ -14,17 +14,18 @@ class PlatformRow
     public:
         PlatformRow(sf::Texture* texture, const unsigned int numPlatforms, const float spacing, const unsigned int gameRow, const bool movingRight);
         ~PlatformRow(){}
-        vector<float> platformPositions(); /*!< Returns a vector of floats containing all the horizontal positions of the platforms
-        within the PlatformRow. */
-        Platform getPlatform(const unsigned int platformNum){ return platforms.at(platformNum-1); } /*!< Returns the Platform object
-        within the row, specified by the input integer. Input integer must be less than or equal to 'const unsigned int numPlatforms'
-        but larger than one. */
-        void changeDirection(); /*!< Reverses the direction of each platform within the PlatfromRow. Direction only changes between
-        left and right. */
-        void changeColour(sf::Texture* texture, bool original); /*!< Changes each platforms colour, within the PlatformRow, to that
-        specified by the Texture Parameter. The bool tracks if each platform is its original colour or not. */
-        void update(float deltaTime); /*!< Updates each platforms', within the PlatformRow, horizontal position using the float parameter. */
-        void draw(sf::RenderWindow& window); /*!< Draws each platform within the PlatformRow with reference to a RenderWindow. */
+        vector<float> platformPositions();
+        /*!< Returns a vector of floats containing all the horizontal positions of the platforms within the PlatformRow. */
+        Platform getPlatform(const unsigned int platformNum){ return platforms.at(platformNum-1); }
+        /*!< Returns the Platform object within the row, specified by the input integer. Input integer must be less than or equal to 'const unsigned int numPlatforms' but larger than one. */
+        void changeDirection();
+        /*!< Reverses the direction of each platform within the PlatfromRow. Direction only changes between left and right. */
+        void changeColour(sf::Texture* texture, bool original);
+        /*!< Changes each platforms colour, within the PlatformRow, to that specified by the Texture Parameter. The bool tracks if each platform is its original colour or not. */
+        void update(float deltaTime);
+        /*!< Updates each platforms', within the PlatformRow, horizontal position using the float parameter. */
+        void draw(sf::RenderWindow& window);
+        /*!< Draws each platform within the PlatformRow with reference to a RenderWindow. */
 
     public:
         bool canChangeColour;
